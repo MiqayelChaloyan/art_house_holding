ac86bca644319459264b8b71d78c882d77da9e6c
MiqayelChaloyan <miqochaloyan@gmail.com>
2024-01-11 14:58:35 +0400

    fix

--- src/components/seo/Meta.tsx
+++ src/components/seo/Meta.tsx
@@ -5,7 +5,7 @@ import { DefaultSeo } from 'next-seo';
 const seoTitle = 'ART House Holding';
 const seoDescription = `Welcome to Art House, your premier destination for transformative learning experiences! At Art House, we pride ourselves on being a dynamic training center that offers a diverse array of courses to ignite your passion and elevate your skills. Our curated curriculum spans a wide spectrum of artistic disciplines, ensuring there's something for everyone seeking to explore and master the realms of creativity.`;
 const seoUrl = 'https://art-house-holding.vercel.app/en';
-const seoOGImage = 'https://images.squarespace-cdn.com/content/v1/59481f98c534a5a32d24c84b/1498621115416-EF1V0P02ZA9SVJP1PGUW/FullSizeRender+%283%29.png?format=1500w';
+const seoOGImage = 'https://images.squarespace-cdn.com/content/v1/63ed0c8e54d11151a02d1803/52c3f2b6-fbf3-469b-880f-bb3181827cc0/Training%2BCenter%2BConference%2BRoom%2BMockup.jpg';
 const seoOGImageAlt = 'ART House Holding';
 const seoSiteName = 'ART House Holding';
 const seoLocale = 'en';
